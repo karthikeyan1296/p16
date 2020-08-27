993c8d2f8edcc2adce093e040b863bfe0bdacbd9
unknown <karthikeyan1296@yahoo.com>
2020-08-27 22:25:42 +0530

    clean method

--- rsk/forms.py
+++ rsk/forms.py
@@ -4,8 +4,7 @@ from django.core.exceptions import ValidationError
 import re
 
 def validate_name(name):
-    m=re.match('[a-zA-Z]+',name)
-    if m.group()!=name:
+    if len(name)>20:
         raise ValidationError("Name is Not Valid")
     return name
 
@@ -29,6 +28,8 @@ class sampleForm(forms.Form):
     
     profile_pic=forms.ImageField(max_length=200,required=True,label="Profile Pic :")
 
+    botcacher=forms.CharField(max_length=10,required=False,widget=forms.HiddenInput)
+
 #    def clean(self,*args,**kwargs):
 #       cleaned_data=super().clean() #getting all the data that is filled in the form
 #       email=cleaned_data.get("email")
@@ -46,3 +47,16 @@ class sampleForm(forms.Form):
             return cleaned_data
         self.add_error('confirm_email',"Incorrect email")#this method will decide to which field we need to show the error
         #self.add_error("fieldname",error message)
+
+    def clean_name(self):
+        name=self.cleaned_data.get("name") #is used to get the name that we have filled in the form
+        for i in name:
+            if not('a'<=i<='z' or 'A'<=i<='Z'):
+                raise ValidationError("Name must contain only alphabets")
+        return name
+
+    def clean_botcacher(self):
+        data=self.cleaned_data.get("botcacher")
+        if len(data)==0:
+            return data
+        self.add_error("name","invalid HUMAN")
